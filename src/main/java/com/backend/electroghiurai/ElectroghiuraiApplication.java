c509f50b6f4306fb55f3b56a46b395a8ecabc34c
RaulStanciu02 <raul.stanciu02@e-uvt.ro>
2023-04-08 11:48:37 +0300

    First Commit

---
+++ src/main/java/com/backend/electroghiurai/ElectroghiuraiApplication.java
@@ -0,0 +1,13 @@
+package com.backend.electroghiurai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroghiuraiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectroghiuraiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/electroghiurai/ElectroghiuraiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.electroghiurai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroghiuraiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
